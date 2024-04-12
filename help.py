4edcd88e517afbd9bc7d588e4c0ccd5a1b23e894
DMGamage <gamagemadhushan@gmail.com>
2024-04-12 12:57:25 +0530

    Basic mlflow methods

---
+++ help.py
@@ -0,0 +1,14 @@
+import mlflow
+mlflow.set_tracking_uri("http:/localhost:5000")
+
+
+exp_id = mlflow.create_experiment('Loan_Prediction')
+
+with mlflow.start_run(run_name='DecisionTreeClass') as run:
+    pass
+
+mlflow.end_run()
+n_estimators=10
+criterion='gini'
+
+mlflow.log_param('n_estimators':n_estimators)
